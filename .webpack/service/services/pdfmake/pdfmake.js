6fe142e1a90e93cad1a39e66311accc50914a04e
NicKAnderson038 <andersonnick038@gmail.com>
2020-12-02 00:17:23 -0500

    ✨

--- .webpack/service/services/pdfmake/pdfmake.js
+++ .webpack/service/services/pdfmake/pdfmake.js
@@ -21,7 +21,62 @@ module.exports =
 /*! runtime requirements: __webpack_require__, __webpack_require__.n, __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"generate\": () => /* binding */ generate\n/* harmony export */ });\n/* harmony import */ var pdfkit__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! pdfkit */ \"pdfkit\");\n/* harmony import */ var pdfkit__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(pdfkit__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _middy_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @middy/core */ \"@middy/core\");\n/* harmony import */ var _middy_core__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_middy_core__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @middy/do-not-wait-for-empty-event-loop */ \"@middy/do-not-wait-for-empty-event-loop\");\n/* harmony import */ var _middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\nconst generatePdf = async () => {\n    return new Promise((resolve, reject) => {\n        const doc = new (pdfkit__WEBPACK_IMPORTED_MODULE_0___default())();\n        doc.text(\"How Are You\");\n        doc.end();\n        const buffers = [];\n        doc.on(\"data\", buffers.push.bind(buffers));\n        doc.on(\"end\", () => {\n            const pdfData = Buffer.concat(buffers);\n            resolve(pdfData);\n        });\n    });\n};\nconst handler = async (event) => {\n    const stream = await generatePdf();\n    return {\n        statusCode: 200,\n        isBase64Encoded: true,\n        headers: {\n            \"Content-type\": \"application/pdf\"\n        },\n        body: stream.toString(\"base64\")\n    };\n};\nconst generate = _middy_core__WEBPACK_IMPORTED_MODULE_1___default()(handler).use(_middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_2___default()());\n\n//# sourceURL=webpack://pdf-maker/./services/pdfmake/pdfmake.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"generate\": () => /* binding */ generate\n/* harmony export */ });\n/* harmony import */ var pdfmake__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! pdfmake */ \"pdfmake\");\n/* harmony import */ var pdfmake__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(pdfmake__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! pdfmake/build/vfs_fonts */ \"pdfmake/build/vfs_fonts\");\n/* harmony import */ var pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _middy_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @middy/core */ \"@middy/core\");\n/* harmony import */ var _middy_core__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_middy_core__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @middy/do-not-wait-for-empty-event-loop */ \"@middy/do-not-wait-for-empty-event-loop\");\n/* harmony import */ var _middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_3__);\n\n\n\n\n// import fontkit from 'fontkit'\n\nconst pdfTemplate = __webpack_require__(/*! ./template.json */ \"./services/pdfmake/template.json\")\nconsole.log(Object.keys((pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1___default().pdfMake.vfs)))\n\nconst generatePDF = async docDefinition => {\n\tconst fontDescriptors = {\n\t\tRoboto: {\n\t\t\tnormal: (pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1___default().pdfMake.vfs[\"Roboto-Regular.ttf\"]),\n\t\t\tbold: (pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1___default().pdfMake.vfs[\"Roboto-Medium.ttf\"]),\n\t\t\titalics: (pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1___default().pdfMake.vfs[\"Roboto-Italic.ttf\"]),\n\t\t\tbolditalics: (pdfmake_build_vfs_fonts__WEBPACK_IMPORTED_MODULE_1___default().pdfMake.vfs[\"Roboto-MediumItalic.ttf\"]),\n\t\t},\n    }\n    // const printer = new pdfMakePrinter(fontDescriptors)\n\t// const doc = printer.createPdfKitDocument(docDefinition)\n\n\treturn new Promise(resolve => {\n        const printer = new (pdfmake__WEBPACK_IMPORTED_MODULE_0___default())(fontDescriptors)\n\t    const doc = printer.createPdfKitDocument(docDefinition)\n\t\tconst chunks = []\n\t\tdoc.end()\n        doc.on('data', chunks.push.bind(chunks))\n        doc.on('end', () => {\n\t\t\tconst pdfData = Buffer.concat(chunks)\n\t\t\tresolve(pdfData)\n        })\n        // doc.on('data', chunk => {\n\t\t// \tchunks.push(chunk)\n\t\t// })\n\t\t// doc.on('end', () => {\n\t\t// \tresolve(Buffer.concat(chunks))\n\t\t// })\n\t})\n}\n\nconst handler = async (context, req) => {\n    // const voucher = context.body\n    const voucher = { \"vendorName\": \"Free Voucher\", \"vendorStreet\": \"123 South Street\", \"vendorZipCity\": \"19103\" }\n\tlet templateStr = JSON.stringify(pdfTemplate)\n\n\ttemplateStr = templateStr.replace('{vendor_name}', voucher.vendorName)\n\ttemplateStr = templateStr.replace('{vendor_street}', voucher.vendorStreet)\n\ttemplateStr = templateStr.replace('{vendor_zip_city}', voucher.vendorZipCity)\n\n\tconst stream = await generatePDF(JSON.parse(templateStr))\n\n\t// context.res = {\n\t// \tbody: stream,\n    // }\n    return {\n\t\tstatusCode: 200,\n\t\tisBase64Encoded: true,\n\t\theaders: {\n\t\t\t'Content-type': 'application/pdf',\n\t\t},\n\t\tbody: stream.toString('base64'),\n\t}\n}\n\nconst generate = _middy_core__WEBPACK_IMPORTED_MODULE_2___default()(handler).use(_middy_do_not_wait_for_empty_event_loop__WEBPACK_IMPORTED_MODULE_3___default()())\n\n//# sourceURL=webpack://pdf-maker/./services/pdfmake/pdfmake.js?");
+
+/***/ }),
+
+/***/ "./services/pdfmake/template.json":
+/*!****************************************!*\
+  !*** ./services/pdfmake/template.json ***!
+  \****************************************/
+/*! default exports */
+/*! export content [provided] [no usage info] [missing usage info prevents renaming] */
+/*!   export 0 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export color [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export fontSize [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export margin [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       export 0 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       export 1 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       other exports [not provided] [no usage info] */
+/*!     export style [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export text [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     other exports [not provided] [no usage info] */
+/*!   export 1 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export color [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export fontSize [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export margin [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       export 0 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       export 1 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       export 2 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       export 3 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!       other exports [not provided] [no usage info] */
+/*!     export style [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export text [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     other exports [not provided] [no usage info] */
+/*!   export 2 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export bold [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export fontSize [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export style [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export text [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     other exports [not provided] [no usage info] */
+/*!   export 3 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export fontSize [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export style [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export text [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     other exports [not provided] [no usage info] */
+/*!   export 4 [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export fontSize [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export style [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     export text [provided] [no usage info] [missing usage info prevents renaming] */
+/*!     other exports [not provided] [no usage info] */
+/*!   other exports [not provided] [no usage info] */
+/*! export pageOrientation [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export pageSize [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: module */
+/***/ ((module) => {
+
+eval("module.exports = JSON.parse(\"{\\\"pageSize\\\":\\\"A4\\\",\\\"pageOrientation\\\":\\\"portrait\\\",\\\"content\\\":[{\\\"text\\\":\\\"Gutschein\\\",\\\"fontSize\\\":27,\\\"color\\\":\\\"#707070\\\",\\\"margin\\\":[0,16],\\\"style\\\":\\\"page\\\"},{\\\"text\\\":\\\"einlösbar bei\\\",\\\"fontSize\\\":15,\\\"color\\\":\\\"#707070\\\",\\\"margin\\\":[0,32,0,4],\\\"style\\\":\\\"page\\\"},{\\\"text\\\":\\\"{vendor_name}\\\",\\\"fontSize\\\":15,\\\"bold\\\":true,\\\"style\\\":\\\"page\\\"},{\\\"text\\\":\\\"{vendor_street}\\\",\\\"fontSize\\\":15,\\\"style\\\":\\\"page\\\"},{\\\"text\\\":\\\"{vendor_zip_city}\\\",\\\"fontSize\\\":15,\\\"style\\\":\\\"page\\\"}]}\");\n\n//# sourceURL=webpack://pdf-maker/./services/pdfmake/template.json?");
 
 /***/ }),
 
@@ -53,17 +108,31 @@ eval("module.exports = require(\"@middy/do-not-wait-for-empty-event-loop\");;\n\
 
 /***/ }),
 
-/***/ "pdfkit":
-/*!*************************!*\
-  !*** external "pdfkit" ***!
-  \*************************/
+/***/ "pdfmake":
+/*!**************************!*\
+  !*** external "pdfmake" ***!
+  \**************************/
 /*! dynamic exports */
 /*! export __esModule [maybe provided (runtime-defined)] [no usage info] [provision prevents renaming (no use info)] */
 /*! other exports [maybe provided (runtime-defined)] [no usage info] */
 /*! runtime requirements: module */
 /***/ ((module) => {
 
-eval("module.exports = require(\"pdfkit\");;\n\n//# sourceURL=webpack://pdf-maker/external_%22pdfkit%22?");
+eval("module.exports = require(\"pdfmake\");;\n\n//# sourceURL=webpack://pdf-maker/external_%22pdfmake%22?");
+
+/***/ }),
+
+/***/ "pdfmake/build/vfs_fonts":
+/*!******************************************!*\
+  !*** external "pdfmake/build/vfs_fonts" ***!
+  \******************************************/
+/*! dynamic exports */
+/*! export __esModule [maybe provided (runtime-defined)] [no usage info] [provision prevents renaming (no use info)] */
+/*! other exports [maybe provided (runtime-defined)] [no usage info] */
+/*! runtime requirements: module */
+/***/ ((module) => {
+
+eval("module.exports = require(\"pdfmake/build/vfs_fonts\");;\n\n//# sourceURL=webpack://pdf-maker/external_%22pdfmake/build/vfs_fonts%22?");
 
 /***/ })
 
--- services/pdfmake/pdfmake.js
+++ services/pdfmake/pdfmake.js
@@ -1,29 +1,65 @@
-import PDFDocument from "pdfkit";
-import middy from "@middy/core";
-import doNotWaitForEmptyEventLoop from "@middy/do-not-wait-for-empty-event-loop";
+import pdfMakePrinter from 'pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+import middy from '@middy/core'
+import doNotWaitForEmptyEventLoop from '@middy/do-not-wait-for-empty-event-loop'
+// import fontkit from 'fontkit'
 
-const generatePdf = async () => {
-    return new Promise((resolve, reject) => {
-        const doc = new PDFDocument();
-        doc.text("How Are You");
-        doc.end();
-        const buffers = [];
-        doc.on("data", buffers.push.bind(buffers));
-        doc.on("end", () => {
-            const pdfData = Buffer.concat(buffers);
-            resolve(pdfData);
-        });
-    });
-};
-const handler = async (event) => {
-    const stream = await generatePdf();
-    return {
-        statusCode: 200,
-        isBase64Encoded: true,
-        headers: {
-            "Content-type": "application/pdf"
-        },
-        body: stream.toString("base64")
-    };
-};
-export const generate = middy(handler).use(doNotWaitForEmptyEventLoop());
+const pdfTemplate = require('./template.json')
+console.log(Object.keys(pdfFonts.pdfMake.vfs))
+
+const generatePDF = async docDefinition => {
+	const fontDescriptors = {
+		Roboto: {
+			normal: pdfFonts.pdfMake.vfs['Roboto-Regular.ttf'],
+			bold: pdfFonts.pdfMake.vfs['Roboto-Medium.ttf'],
+			italics: pdfFonts.pdfMake.vfs['Roboto-Italic.ttf'],
+			bolditalics: pdfFonts.pdfMake.vfs['Roboto-MediumItalic.ttf'],
+		},
+	}
+	// const printer = new pdfMakePrinter(fontDescriptors)
+	// const doc = printer.createPdfKitDocument(docDefinition)
+
+	return new Promise(resolve => {
+		const printer = new pdfMakePrinter(fontDescriptors)
+		const doc = printer.createPdfKitDocument(docDefinition)
+		const chunks = []
+		doc.end()
+		doc.on('data', chunks.push.bind(chunks))
+		doc.on('end', () => {
+			const pdfData = Buffer.concat(chunks)
+			resolve(pdfData)
+		})
+		// doc.on('data', chunk => {
+		// 	chunks.push(chunk)
+		// })
+		// doc.on('end', () => {
+		// 	resolve(Buffer.concat(chunks))
+		// })
+	})
+}
+
+const handler = async (context, req) => {
+	const voucher = context.body
+	// const voucher = { "vendorName": "Free Voucher", "vendorStreet": "123 South Street", "vendorZipCity": "19103" }
+	let templateStr = JSON.stringify(pdfTemplate)
+
+	templateStr = templateStr.replace('{vendor_name}', voucher.vendorName)
+	templateStr = templateStr.replace('{vendor_street}', voucher.vendorStreet)
+	templateStr = templateStr.replace('{vendor_zip_city}', voucher.vendorZipCity)
+
+	const stream = await generatePDF(JSON.parse(templateStr))
+
+	// context.res = {
+	// 	body: stream,
+	// }
+	return {
+		statusCode: 200,
+		isBase64Encoded: true,
+		headers: {
+			'Content-type': 'application/pdf',
+		},
+		body: stream.toString('base64'),
+	}
+}
+
+export const generate = middy(handler).use(doNotWaitForEmptyEventLoop())
